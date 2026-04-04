2971241df6bbd78750373874a7017feaf6828903
Delwix <delwinmateo.gauson@cit.edu>
2026-04-04 12:44:58 +0800

    Fix Bulwark of Righteous

--- DefensiveYell.lua
+++ DefensiveYell.lua
@@ -10,31 +10,34 @@ local spells = {
     ["Shield Wall"] = true,
     -- Shaman, this is sad
     -- Paladin
-    ["Bulwark of Righteous"] = true
+    ["Bulwark of the Righteous"] = true 
 }
 
 local active = {}
 
 frame:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
 frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
+frame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
 frame:RegisterEvent("CHAT_MSG_SPELL_AURA_GONE_SELF")
 
 frame:SetScript("OnEvent", function()
     local msg = arg1
 
-    if event == "CHAT_MSG_SPELL_SELF_BUFF" or event == "CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS" then
+    if event == "CHAT_MSG_SPELL_SELF_BUFF" or
+       event == "CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS" or
+       event == "CHAT_MSG_SPELL_SELF_DAMAGE" then
+
         for spellName in pairs(spells) do
             if string.find(msg, spellName) then
-                
                 if not active[spellName] then
                     SendChatMessage(spellName .. " up!", "YELL")
                     active[spellName] = true
                 end
-
             end
         end
     end
 
+    -- Buff/ability fades
     if event == "CHAT_MSG_SPELL_AURA_GONE_SELF" then
         for spellName in pairs(spells) do
             if string.find(msg, spellName) then
